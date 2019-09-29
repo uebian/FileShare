4fef5593ad98bb9dd0fc086d64df5bbec54cb637
uebian <2452973529@qq.com>
2019-09-29 12:39:52 +0000

    用jquery重写部分代码

--- app/src/main/java/net/newlydev/fileshare_android/Utils.java
+++ app/src/main/java/net/newlydev/fileshare_android/Utils.java
@@ -144,31 +144,24 @@ public class Utils
 		String s = new String(ob);
 		return s;
 	}
-	
+
+	//产生随机字符串
 	public static String getRandomString(int length){
-		//产生随机数
 		Random random=new Random();
 		StringBuffer sb=new StringBuffer();
-		//循环length次
 		for(int i=0; i<length; i++){
-			//产生0-2个随机数，既与a-z，A-Z，0-9三种可能
 			int number=random.nextInt(3);
 			long result=0;
 			switch(number){
-					//如果number产生的是数字0；
 				case 0:
-					//产生A-Z的ASCII码
 					result=Math.round(Math.random()*25+65);
-					//将ASCII码转换成字符
 					sb.append(String.valueOf((char)result));
 					break;
 				case 1:
-					//产生a-z的ASCII码
 					result=Math.round(Math.random()*25+97);
 					sb.append(String.valueOf((char)result));
 					break;
 				case 2:
-					//产生0-9的数字
 					sb.append(String.valueOf
 							  (new Random().nextInt(10)));
 					break; 
